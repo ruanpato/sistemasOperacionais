7fd53f56cd82d5ab5fb7d06494773ec3b02423ee
ruanpato <ruanpato@outlook.com>
2020-11-03 08:38:03 -0300

    Fixed with Idle wait

--- trabalhos/1/deadlock.c
+++ trabalhos/1/deadlock.c
@@ -5,8 +5,6 @@
 void *thread1(void *data);
 void *thread2(void *data);
 
-
-
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER, mutex2 = PTHREAD_MUTEX_INITIALIZER;
 pthread_t Thread1, Thread2;
 
@@ -21,64 +19,46 @@ int main(void) {
 
 
     return 1;
-    
-
 }
 
 void *thread1(void *data){
     unsigned long i,j;
-    if(pthread_mutex_trylock(&mutex1)==0){
+    if(pthread_mutex_lock(&mutex1) == 0) {
         printf("Thread ID%ld got mutex1.\n", pthread_self());
         for(i=0; i< 10000000; ++i); // just for wasting some time
-        while(pthread_mutex_trylock(&mutex2) != 0) {
-            pthread_mutex_unlock(&mutex2);
+        while(1) {
+            pthread_mutex_unlock(&mutex1); // If can't get needs free the used mutex, and if get need's free too
+            if(pthread_mutex_trylock(&mutex2) == 0) {
+                printf("Thread ID%ld got mutex2.\n", pthread_self());          
+                for(i=0; i< 10000000; ++i); // just for wasting some time
+                pthread_mutex_unlock(&mutex2);
+                break;
+            }
         }
-        printf("Thread ID%ld got mutex2.\n", pthread_self()); 
-        /*if(pthread_mutex_trylock(&mutex2)==0){
-            printf("Thread ID%ld got mutex2.\n", pthread_self());          
-            for(i=0; i< 10000000; ++i); // just for wasting some time
-            pthread_mutex_unlock(&mutex2);   
-        } else {
-            printf("\nThread ID%ld did not get mutex2.\n", pthread_self());
-            pthread_mutex_unlock(&mutex2);
-        }*/
-        pthread_mutex_unlock(&mutex1);
-     } else {
+    } else { 
         printf("\nThread ID%ld did not get mutex1.\n", pthread_self());
         pthread_mutex_unlock(&mutex1);
+        pthread_exit(NULL);
     }
-    pthread_exit(NULL);
 }
 
 void *thread2(void *data){
     unsigned long i,j;
-    if(pthread_mutex_lock(&mutex2)==0){
+    if(pthread_mutex_lock(&mutex2) == 0) {
         printf("Thread ID%ld got mutex2.\n", pthread_self());
         for(i=0; i< 10000000; ++i); // just for wasting some time
-
-        while(pthread_mutex_trylock(&mutex1) != 0) {
-            pthread_mutex_unlock(&mutex1);
+        while(1) {
+            pthread_mutex_unlock(&mutex2); // If can't get needs free the used mutex, and if get need's free too
+            if(pthread_mutex_trylock(&mutex1) == 0) {
+                printf("Thread ID%ld got mutex1.\n", pthread_self());          
+                for(i=0; i< 10000000; ++i); // just for wasting some time
+                pthread_mutex_unlock(&mutex1);
+                break;
+            }
         }
-        printf("Thread ID%ld got mutex1.\n", pthread_self()); 
-        /*
-        if(pthread_mutex_lock(&mutex1)==0){
-            printf("Thread ID%ld got mutex1.\n", pthread_self());          
-            for(i=0; i< 10000000; ++i); // just for wasting some time
-            pthread_mutex_unlock(&mutex1);
-        } else {
-            printf("\nThread ID%ld did not get mutex1.\n", pthread_self());
-            pthread_mutex_unlock(&mutex1);
-        }*/
-        pthread_mutex_unlock(&mutex2);
-    } else {
+    } else { 
         printf("\nThread ID%ld did not get mutex2.\n", pthread_self());
         pthread_mutex_unlock(&mutex2);
+        pthread_exit(NULL);
     }
-    pthread_exit(NULL);
 }
-
-
-
-
-
-
